063bf8b097cf79e0df885f20298ec4623d6ab0e2
Murilo Sousa <murlsousa@gmail.com>
2022-03-20 20:55:01 +0100

    Added put and delete functions. Finished with task CRUD endpoint.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, abort, reqparse
 
 from src.task import TaskDAOMock
 
@@ -9,30 +9,73 @@ api = Api(app)
 
 taskDAOMock = TaskDAOMock()
 
-class TaskEndpoint(Resource):
+def title_validation():
+    def validate(value):
+        if len(value) < 3:
+            raise ValueError(f"Value must be at least three characters.")
+        return value
+        
+    return validate
     
-    # TODO: Look into reqparse?
-    # Example:
-    # parser = reqparse.RequestParser()
-    # parser.add_argument('title',
-    #     type=str,
-    #     required=True,
-    #     help = "A task must have at least 3 characters."
-    # )
-    # USAGE:
-    # data = TaskEndpoint.parser.parse_args()
- 
+parser = reqparse.RequestParser()
+parser.add_argument('title',
+    type=title_validation(),
+    required=True,
+    help = "A task must have at least 3 characters.",
+    trim=True
+)
+parser.add_argument('done',
+    type=bool,
+    help = "A task must have at least 3 characters.",
+)
+
+def abort_if_task_not_found(id : int):
+    task = taskDAOMock.retrieve_task(id)
+    if not task:
+        abort(404, message=f"Task with id {id} doesn't exist.")
+    
+    return task
+
+class TasksEndpoint(Resource):
+    
     def get(self):
         tasks = taskDAOMock.retrieve_tasks()
         return {'tasks':list(x.encode_json() for x in tasks)}
- 
+
     def post(self):
         data = request.get_json()
+        data = parser.parse_args()
+
         new_task = taskDAOMock.persist_task(data['title'])
         
         return new_task.encode_json(), 201
 
-api.add_resource(TaskEndpoint, '/api/task')
+class TaskEndpoint(Resource):
+
+    def get(self, id : int):
+        task = abort_if_task_not_found(id)
+
+        return task.encode_json()
+    
+    def put(self, id : int):
+        data = parser.parse_args()
+        task = abort_if_task_not_found(id)
+        task.title = data['title']
+        if data['done']:
+            task.done = data['done']
+
+        taskDAOMock.update_task(task)
+        return task.encode_json(), 200
+    
+    def delete(self, id : int):
+        abort_if_task_not_found(id)
+        if taskDAOMock.delete_task(id):
+            return None, 204
+        return "¯\\_(ツ)_/¯", 404
+
+
+api.add_resource(TaskEndpoint, '/api/task/<int:id>')
+api.add_resource(TasksEndpoint, '/api/task')
 
 app.debug = True
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+from turtle import title
 import pytest
 import json
 
@@ -12,7 +13,7 @@ def client_fxt():
         yield client
 
 @pytest.fixture
-def existing_task_fxt(client_fxt):
+def existing_task_fxt():
     task = Task("Simple task")
     taskDAOMock.persist_task(task)
 
@@ -27,12 +28,20 @@ def test_get_tasks(client_fxt, existing_task_fxt):
     assert next((x for x in responseData['tasks'] if existing_task_fxt.id == x['id']), None)
 
 def test_get_task(client_fxt, existing_task_fxt):
-    response = client_fxt.get('/api/task', query_string={ 'id' = existing_task_fxt.id})
-    responseData = json.loads(response.data.decode())
+    response = client_fxt.get(f'/api/task/{existing_task_fxt.id}')
     assert response.status_code == 200
+    responseData = json.loads(response.data.decode())
     assert responseData
-    assert isinstance(responseData['tasks'], list)
-    assert next((x for x in responseData['tasks'] if existing_task_fxt.id == x['id']), None)
+    task = Task.decode_json(responseData)
+
+    assert task
+    assert task.title == existing_task_fxt.title
+    assert task.done == existing_task_fxt.done
+    assert task.id == existing_task_fxt.id
+
+def test_get_non_existing_task(client_fxt):
+    response = client_fxt.get(f'/api/task/0')
+    assert response.status_code == 404
 
 def test_create_task(client_fxt):
     taskTitle = 'Testing tasks post endpoint'
@@ -43,10 +52,10 @@ def test_create_task(client_fxt):
             )),
             content_type='application/json',
         )
-    responseData = json.loads(response.data.decode())
 
     assert response.status_code == 201
 
+    responseData = json.loads(response.data.decode())
     task = Task.decode_json(responseData)
 
     assert task
@@ -55,7 +64,39 @@ def test_create_task(client_fxt):
     assert not task.done
 
 def test_create_task_no_title(client_fxt):
-    taskTitle = ''
+    response = client_fxt.post(
+            '/api/task',
+            data=json.dumps(dict(
+            )),
+            content_type='application/json',
+        )
+    
+    assert response.status_code == 400
+
+def test_update_task_title(client_fxt, existing_task_fxt):
+    previousTitle = existing_task_fxt
+    response = client_fxt.put(
+            f'/api/task/{existing_task_fxt.id}',
+            data=json.dumps(dict(
+                title='update me!',
+                done=not existing_task_fxt.done
+            )),
+            content_type='application/json',
+        )
+    
+    assert response.status_code == 200
+
+    responseData = json.loads(response.data.decode())
+    task = Task.decode_json(responseData)
+    assert task
+    assert task.title == 'update me!'
+    assert task.id == existing_task_fxt.id
+    assert task.done != existing_task_fxt.done
+
+def test_delete_task(client_fxt):
+    # assume task creation is working since this is tested elsewhere.
+
+    taskTitle = 'Testing tasks post endpoint'
     response = client_fxt.post(
             '/api/task',
             data=json.dumps(dict(
@@ -63,6 +104,13 @@ def test_create_task_no_title(client_fxt):
             )),
             content_type='application/json',
         )
-    
-    assert response.status_code == 201
 
+    responseData = json.loads(response.data.decode())
+    task = Task.decode_json(responseData)
+
+    assert task
+    assert task.id
+
+    responseDelete = client_fxt.delete(f'/api/task/{task.id}')
+
+    assert responseDelete.status_code == 204
